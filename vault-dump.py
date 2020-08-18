a043f58a09772941aab0dde1329583caeb1c19c4
asozaev <40871546+asozaev@users.noreply.github.com>
2020-08-18 11:14:53 +0300

    Update vault-dump.py

--- vault-dump.py
+++ vault-dump.py
@@ -43,25 +43,28 @@ def print_header():
 
 # looks at an argument for a value and prints the key
 #  if a value exists
-def recurse_for_values(path_prefix, candidate_key):
+def recurse_for_values(path_meta_prefix, candidate_key):
     candidate_values = candidate_key['data']['keys']
     for candidate_value in candidate_values:
-        next_index = path_prefix + candidate_value
+        next_index = path_meta_prefix + candidate_value
         if candidate_value.endswith('/'):
             next_value = client.list(next_index)
             recurse_for_values(next_index, next_value)
         else:
-            stripped_prefix=path_prefix[:-1]
-            final_dict = client.read(next_index)['data']
-            print ("\nvault write {}".format(next_index), end='')
+            next_index = next_index.replace("metadata", "data")
+            stripped_prefix=path_meta_prefix[:-1]
+            final_dict = client.read(next_index)['data']['data']
+            next_index = next_index.replace("data/", "")
+            print ("\nvault kv put {}".format(next_index), end='')
 
             sorted_final_keys = sorted(final_dict.keys())
             for final_key in sorted_final_keys:
                 final_value = final_dict[final_key]
-                try:
-                    final_value = final_value.encode("utf-8")
-                except AttributeError:
-                    final_value = final_value
+      #          try:
+      #              final_value = final_value.encode("utf-8")
+      #          except AttributeError:
+      #              final_value = final_value
+                #print (" {0}={1}".format(final_key, repr(str(final_value, "utf-8"))), end='')
                 print (" {0}={1}".format(final_key, repr(final_value)), end='')
 
 
@@ -79,8 +82,7 @@ hvac_client = {
 client = hvac.Client(**hvac_client)
 assert client.is_authenticated()
 
-top_vault_prefix = os.environ.get('TOP_VAULT_PREFIX','/secret/')
-
+top_vault_meta_prefix = os.environ.get('TOP_VAULT_META_PREFIX','/secret/metadata/')
 print_header()
-top_level_keys = client.list(top_vault_prefix)
-recurse_for_values(top_vault_prefix, top_level_keys)
+top_level_keys = client.list(top_vault_meta_prefix)
+recurse_for_values(top_vault_meta_prefix, top_level_keys)
